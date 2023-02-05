9ccf0f2ff1bd8e0d2f79e859181beb23cb1470a8
nouman56 <noumanafzaal56@gmail.com>
2023-02-05 13:18:33 +0500

    User Service complete assignment with different properties files

--- src/main/java/com/ascertia/userserviceclient/UserServiceClientApplication.java
+++ src/main/java/com/ascertia/userserviceclient/UserServiceClientApplication.java
@@ -2,13 +2,17 @@ package com.ascertia.userserviceclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@PropertySource("classpath:application-stg.properties")
 public class UserServiceClientApplication {
 
     public static void main(String[] args) {
